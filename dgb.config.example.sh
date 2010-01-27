80089e1995f65f952cf4da1a8f4bd851d226baeb
scor <scorlosquet@gmail.com>
2010-01-27 15:46:57 -0500

    renamed drbaksh to dgb.sh added support for custom config file

---
+++ dgb.config.example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# rename to dgb.config.sh and these settings will be used by dgb.sh
+
+# Absolute path to the drush executable.
+#DRUSH="/home/scor/.drush/drush/drush"
+
+# Absolute path to the backup directory
+#BACKUP="/home/scor/drupal_sites/multi6"
+
+# Absolute path to the root folder of the Drupal code base (leave it as it is
+# unless you have a specific path for it).
+#DRUPAL_ROOT="$BACKUP/drupal"
+
