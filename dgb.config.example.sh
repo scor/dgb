a2878efa2fc9f34e6bd804593f58df18779df2f6
scor <scorlosquet@gmail.com>
2010-02-04 17:40:59 -0500

    more docs fixes

--- dgb.config.example.sh
+++ dgb.config.example.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# rename to dgb.config.sh and these settings will be used by dgb.sh
+# Rename this file to dgb.config.sh and configure the paths below to match your
+# system. This is required for the main dgb.sh script to work.
 
 # Absolute path to the drush executable.
 DRUSH="/home/scor/.drush/drush/drush"
@@ -8,11 +9,11 @@ DRUSH="/home/scor/.drush/drush/drush"
 # Absolute path to the backup directory.
 BACKUP="/var/sites/drupal_sites/multi6"
 
-# Absolute path to the folder containing the dgb script. Assumes it's one level
+# Path to the folder containing the dgb script. Assumes it's one level
 # up the backup directory by default.
-# (leave it as it is unless it has a specific).
+# (leave it as it is unless it has a different path).
 DGB="$BACKUP/.."
 
-# Absolute path to the root folder of the Drupal code base
-# (leave it as it is unless it has a specific).
+# Path to the root folder of the Drupal code base
+# (leave it as it is unless it has a different path).
 DRUPAL_ROOT="$BACKUP/drupal"
